1e6521a1205bdbf2f778c78c76995cbe4cb3f90e
domador1711 <deportesdavid17@gmail.com>
2024-09-21 21:21:59 -0500

    new file:   public/fonts/CircularStd-Black.woff2 	new file:   public/fonts/CircularStd-Bold.woff2 	new file:   public/fonts/CircularStd-Book.woff2 	new file:   public/fonts/CircularStd-Light.woff2 	new file:   public/fonts/CircularStd-Medium.woff2 	new file:   public/music/1/01.mp3 	new file:   public/music/1/02.mp3 	new file:   public/music/1/03-Closer.mp3 	new file:   public/music/1/Ozuna - El Farsante (Audio Oficial)  Odisea.mp3 	modified:   src/components/AsideMenu.astro 	deleted:    src/components/Card.astro 	new file:   src/components/Greeting.svelte 	new file:   src/components/Player.jsx 	new file:   src/components/PlaylistItemCard.astro 	new file:   src/components/SideMenuCard.astro 	modified:   src/components/SideMenuItem.astro 	modified:   src/layouts/Layout.astro 	new file:   src/lib/colors.ts 	new file:   src/lib/data.ts 	modified:   src/pages/index.astro 	new file:   src/pages/playlist/[id].astro 	new file:   svelte.config.js 	modified:   tsconfig.json

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script langs="ts">
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+
+    let greeting= ""
+
+    if (currentHour < 12) {
+        greeting = 'Buenos Dias'
+    } else if (currentHour < 18) {
+        greeting = 'Buenas Tardes'
+    } else {
+        greeting = 'Buenas Noches'
+    }
+</script>
+
+<h1 class="text-3xl font-bold text-white">{greeting}</h1>
